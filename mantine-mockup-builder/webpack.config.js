2ab2ff1819ad5c4432299562b366f13a51d65537
James H. <lsaucyfx@gmail.com>
2024-03-22 22:16:50 -0400

    updated webpack config and package.json, fixed bugs

--- mantine-mockup-builder/webpack.config.js
+++ mantine-mockup-builder/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path'):
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
 		    {
 			    test: /\.js$|jsx/,
 			    exclude: /node_modules/,
-			    useL 'babel-loader'
+			    use: 'babel-loader'
 		    },
 		    {
 			    test: /\.css$/,
